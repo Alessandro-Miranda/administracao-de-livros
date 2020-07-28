b55f02043cf10cc0e3d7f51ac8ef5750314a116d
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2020-07-28 18:52:10 -0300

    Script para requisição primária ao banco para exibir todos os livros já cadastrados

--- php/index.php
+++ php/index.php
@@ -1,14 +1,22 @@
 <?php
+    header('Content-Type: application/json; charset=UTF-8');
+    
     $username = 'root';
     $password = 'gugaBB05!8';
+    $queryInstruction = 'SELECT * FROM livros';
+    $result = '';
+
     try
     {
         $connection = new PDO('mysql:host=127.0.0.1:3306; dbname=livros', $username, $password);
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo 'conectou no bd';
+        $connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $result = $connection -> query($queryInstruction);
+
+        echo json_encode($result -> fetchAll(PDO::FETCH_ASSOC));
     }
     catch(PDOException $e)
     {
-        echo "Error: ".$e->getMessage();
+        echo json_encode(array('Error: '=>$e->getMessage()));
     }
+    
 ?>
