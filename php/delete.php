237448bedce3d4ac6d52fadbeb6189b1886b9aed
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2020-07-28 19:35:29 -0300

    Script php para delete de livro

--- php/delete.php
+++ php/delete.php
@@ -1,23 +1,26 @@
 <?php
     header('Content-Type: application/json; charset=UTF-8');
     
-    echo json_encode(array("Deletou?" => "obvio fdp"));
-    // $username = 'root';
-    // $password = 'gugaBB05!8';
-    // $queryInstruction = 'SELECT * FROM livros';
-    // $result = '';
+    $nome = $_GET['nome'];
+    $username = 'root';
+    $password = 'gugaBB05!8';
+    $queryInstruction = 'DELETE FROM livros WHERE nome = :nome';
+    $result = '';
 
-    // try
-    // {
-    //     $connection = new PDO('mysql:host=127.0.0.1:3306; dbname=livros', $username, $password);
-    //     $connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //     $result = $connection -> query($queryInstruction);
+    try
+    {
+        $connection = new PDO('mysql:host=127.0.0.1:3306; dbname=livros', $username, $password);
+        $connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $result = $connection -> prepare($queryInstruction);
+        $result -> execute(array(
+            ':nome' => $nome
+        ));
 
-    //     echo json_encode($result -> fetchAll(PDO::FETCH_ASSOC));
-    // }
-    // catch(PDOException $e)
-    // {
-    //     echo json_encode(array('Error: '=>$e->getMessage()));
-    // }
+        echo json_encode($result -> rowCount());
+    }
+    catch(PDOException $e)
+    {
+        echo json_encode(array('Error: '=>$e->getMessage()));
+    }
     
 ?>
