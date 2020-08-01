44a5bf929f09cb42233e257f9439d06c585b3351
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2020-08-01 19:59:31 -0300

    Atualizado o utf8 do BD para ser compatível com a importação do phpmyadmin

--- data/livros_livros.sql
+++ data/livros_livros.sql
@@ -31,7 +31,7 @@ CREATE TABLE `livros` (
   `data_inclusao_edicao` date DEFAULT NULL,
   PRIMARY KEY (`nome`),
   UNIQUE KEY `nome_UNIQUE` (`nome`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
